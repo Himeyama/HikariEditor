7678a020311ff58dadba0249f1f5bca5a87cff52
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2023-06-04 04:17:40 +0900

    TeX の記号を一部実装

---
+++ HikariEditor/Preview/PDF.xaml.cs
@@ -0,0 +1,30 @@
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+
+namespace HikariEditor
+{
+    public sealed partial class PDF : Page
+    {
+        MainWindow mainWindow;
+        FileItem fileItem;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            PDFPageInfo pdfPageInfo = e.Parameter as PDFPageInfo;
+            mainWindow = pdfPageInfo.mainWindow;
+            fileItem = pdfPageInfo.fileItem;
+
+            WebView.Source = new Uri(fileItem.Path);
+
+            base.OnNavigatedTo(e);
+        }
+
+        public PDF()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
